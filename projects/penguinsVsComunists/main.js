e508ff12895b090fcf9f5cc32aedc313f5dd2e9e
Tanner Ward <hoserehh@gmail.com>
2016-05-18 10:29:27 -0600

    fixed bug in P.V.C.

--- projects/penguinsVsComunists/main.js
+++ projects/penguinsVsComunists/main.js
@@ -13,7 +13,7 @@ function onHit(party) {
         penguins.population -= (Math.floor(Math.random() * 1000000) + 1);
         console.log("nuke hits " + penguins.name);
         console.log("Penguin population " + penguins.population);
-        
+
         if (penguins.population > 0) {
             sendNuke(penguins, onHit, onMiss);
             console.log(penguins.name + " sending nuke");
@@ -22,12 +22,13 @@ function onHit(party) {
         comunists.population -= (Math.floor(Math.random() * 1000000) + 1);
         console.log(" nuke hits " + comunists.name);
         console.log("Comie population " + comunists.population);
-        sendNuke(comunists, onHit, onMiss);
+        
         if (comunists.population > 0) {
             console.log(comunists.name + " sending nuke");
+            sendNuke(comunists, onHit, onMiss);
         }
     }
-};
+}
 
 function onMiss(party) {
     if (party === penguins) {
@@ -35,16 +36,16 @@ function onMiss(party) {
         console.log(comunists.name + " sending nuke");
         sendNuke(comunists, onHit, onMiss);
     } else
+        if (party === comunists){
         console.log(comunists.name + "'s nuke missed");
     console.log(penguins.name + " sending nuke");
     sendNuke(penguins, onHit, onMiss);
+        }
 
 
-
-};
+}
 
 function sendNuke(party, onHit, onMiss) {
-    //    console.log(party.name + " sending nuke");
     setTimeout(function () {
 
 
@@ -63,16 +64,16 @@ function sendNuke(party, onHit, onMiss) {
     }, 1)
 
 
-};
+}
 
 
 function start() {
     var maths = (Math.floor(Math.random() * 2) + 1);
-    if (maths = 1) {
+    if (maths === 1) {
         console.log(penguins.name + " wage ware on the comunists")
         sendNuke(penguins, onHit, onMiss);
     } else
         console.log(comunists.name + " wage war on the penguins")
     sendNuke(comunists, onHit, onMiss);
-};
+}
 start();
