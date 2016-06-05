fcc60f4d1c7f898260d336d1c7f3c5c072064d53
Tanner Ward <hoserehh@gmail.com>
2016-06-05 15:58:06 -0600

    style update

--- projects/MyLittleBrony/app.js
+++ projects/MyLittleBrony/app.js
@@ -1,26 +1,34 @@
 var app = angular.module("MainApp", ["ngRoute"]);
 
-app.config(function($routeProvider) {
-        $routeProvider
+app.config(function ($routeProvider) {
+    $routeProvider
         .when("/add", {
-        templateUrl:"veiws/add.html",
-    }).otherwise({
-            templateUrl:"veiws/home.html"
+            templateUrl: "veiws/add.html",
+        }).otherwise({
+            templateUrl: "veiws/home.html"
         })
-    
+
 })
 
-app.controller("mainController", ["$scope", "ponyHttp", function($scope, ponyHttp) {
-   $scope.ponies = [];
-    ponyHttp.getPony().then(function(ponys) {
-            $scope.ponies = ponys; 
+app.controller("mainController", ["$scope", "ponyHttp", function ($scope, ponyHttp) {
+    $scope.tryAgain = false;
+    $scope.ponies = [];
+    ponyHttp.getPony().then(function (ponys) {
+        $scope.ponies = ponys;
         console.log($scope.ponies)
     })
-    $scope.addPony = function(pony) {
-        ponyHttp.addPony(pony);
-        $scope.pony = {};
+    $scope.addPony = function (pony) {
+        if (pony.name.length > 3 && pony.imgUrl.length > 1 && pony.username.length > 0 ) {
+            ponyHttp.addPony(pony);
+            $scope.pony = {};
+        } else {
+            $scope.tryAgain = true
+        }
+
     }
-    $scope.deleteOne = function(pony) {
-        ponyHttp.removePony(pony)
+    $scope.deleteOne = function (pony) {
+        if (confirm("This is perminate are you sure?") === true) {
+            ponyHttp.removePony(pony)
+        } else {}
     }
 }])
