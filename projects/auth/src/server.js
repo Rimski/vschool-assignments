841f6ef3d1f0bbb7aeed725e28ec29857076937b
Tanner Ward <hoserehh@gmail.com>
2016-06-24 14:44:44 -0600

    started new project

--- projects/auth/src/server.js
+++ projects/auth/src/server.js
@@ -3,21 +3,64 @@ var app = express();
 var bodyParser = require("body-parser");
 var morgan = require("morgan");
 var cors = require("cors");
-var mongoose = require("mongoose");
 var port = process.env.port || 8000;
 var path = require("path");
-
 app.use(cors());
 app.use(bodyParser.json());
-mongoose.connect("mongodb://localhost/");
-mongoose.connection.on("connected", function() {
-    console.log("COUNTRYSIDE");
+//app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "..", "public")));
+//postgres schema tabel stuff
+var jugglingdb = require("jugglingdb");
+var Schema = jugglingdb.Schema;
+var schema = new Schema("postgres", {
+    database: "TheBest",
+    username: "TheBest",
+    host: "localhost",
+    port: 5432
+});
+var _User = schema.define("_User", {
+    name: {type: String, required: true},
+    date:      { type: Date,    default: function () { return new Date;} },
+    timestamp: { type: Number,  default: Date.now },
+    age: Number
+});
+var _Post = schema.define('_Post', {
+    title: String,
+    description: String,
+    date:      { type: Date,    default: function () { return new Date;} },
+    timestamp: { type: Number,  default: Date.now },
 });
 
-app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "..", "public")));
+_User.hasMany(_Post, {as: 'posts', foreignKey: "userId"});
+_Post.belongsTo(_User, {as: "author", foreignKey: "userid"});
+
+
+
+schema.automigrate();
+// routes 
+app.post("/users", function(req, res) {
+    var user = new _User(req.body);
+    user.save(function(err) {
+//        if (err) return res.status(500).send("there was a problem: " + err);
+//         res.send(item);
+        res.send(user);
+    console.log(user);
+    });
+});
+app.post("/posts", function(req, res) {
+    var post = new _Post(req.body);
+    post.save(function(err, item) {
+        if (err) return res.status(500).send("there was a problem: " + err);
+         res.send(item);
+    })
+});
+app.get("/users", function(req, res) {
+   
+    res.send(schema.models._User);
+})
+
 
 
 app.listen(port, function() {
-    console.log("BURNINATING THE ")
+    console.log("BURNINATING THE " + port)
 });
